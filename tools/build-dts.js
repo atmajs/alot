7164a6ef44413e2c7706c89c6215e4ba886cf29b
Alex Kit <source.kit@gmail.com>
2019-12-10 13:44:39 +0100

    fix (dts)

--- tools/build-dts.js
+++
@@ -1,9 +0,0 @@
-var dts = require('dts-bundle');
-
-dts.bundle({
-	name: 'alot',
-	main: './ts-temp/export.d.ts',
-	out: './typings/index.d.ts'
-});
-
-io.File.copyTo('./ts-temp/typings/index.d.ts', './lib/alot.d.ts');
